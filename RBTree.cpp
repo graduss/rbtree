a4df8e0825df84bb28f2124da211a12812f1831d
sasha <graduss@tut.by>
2014-02-28 23:01:02 +0300

    added method insert;

--- RBTree.cpp
+++ RBTree.cpp
@@ -61,22 +61,47 @@ Node* RBTree::btInsert(const Node* n){
     return n;
 }
 
-Node* RBTree::insert(const Node* n){
-    btInsert(n);
-    n->red = true;
+Node* RBTree::insert(const Node* x){
+    btInsert(x);
+    x->red = true;
 
     Node* y;
-    while (n !== root && n->p->red){
-        if(x->p == x->p->l){
-            y = x->p->r;
-            if(y->red){
+    while (x !== root && x->p->red){
+        if(x->p == x->p->p->l){ // родитель является левым ребенком.
+            y = x->p->p->r;
+            if(y && y->red){ // случей 1 (I)
                 x->p->red = false;
                 y->red = false;
                 x->p->p->red = true;
                 x = x->p->p;
+            }else {
+                if (x == x->p->r) { // случей 2 (I) -> случей 3 (I)
+                    x = x->p;
+                    leftRotate(x);
+                }
+                x->p->red = false; // случей 3 (I)
+                x->p->p->reg = true;
+                rightRotate(x->p->p);
+            }
+        }else{ // родитель является правым ребенком.
+            y = x->p->p->l;
+            if(Y && y->red){ // случей 1 (II)
+                x->p->red = false;
+                y->red = false;
+                x->p->p = true;
+                x = x->p->p;
+            }else{
+                if(x == x->p->r){ // случей 2 (II) -> случей 3 (II)
+                    x = x->p;
+                    rightRotate(x);
+                }
+                x->p->red = false; // сулчей 3 (II)
+                x->p->p->red = true;
+                leftRotate(x->p->p);
             }
         }
     }
+    root->red = false;
 }
 
 }
