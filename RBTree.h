f3fddf0854acf431a3fa4a4501b0e24d06281a8c
Sasha <graduss@tut.by>
2014-03-13 08:01:40 +0300

    change class to template class attach SmartPointer

--- RBTree.h
+++ RBTree.h
@@ -1,12 +1,50 @@
-#include "Node.h"
 #include <iostream>
+#include "smartpointer/SPointer.h"
 
 #ifndef RBTREE_H
 #define RBTREE_H
 
 namespace rbt{
 
-class RBTree
+template <class Data>
+class RBTree {
+
+public:
+    RBTree(): root(0) {}
+    ~RBTree();
+
+    RBTree& add(const Data& key);
+    RBTree& del(const Data& key);
+
+    Data& successor(const Data& key) const;
+    Data& predecessor(const Data& key) const;
+    Data& min(const Data& key) const;
+    Data& max(const Data& key) const;
+
+private:
+    struct Item{
+        Data key;
+        SPointer<Item> l, r, p;
+
+        Item(Data& k): key(k), l(0), r(0), p(0) {}
+        ~Item();
+    };
+    SPointer<Item> root;
+};
+
+template <class Data>
+RBTree<Data>::Item::~Item(){
+    if(p){
+        if(p->l == this) p->l = 0;
+        else p->r = 0;
+    }
+
+    if(l) delete &l;
+    if(r) delete &r;
+    p = l = r = 0;
+}
+
+/*class RBTree
 {
     public:
         RBTree();
@@ -31,7 +69,7 @@ class RBTree
         Node* btInsert(Node* n);
         void rmoveFixup(Node* x);
         void inorderTreeWalk(const Node *n, int h = 0) const;
-};
+};*/
 
 };
 
