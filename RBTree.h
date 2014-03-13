a7c794d52b0f91871984e4d75d56bb4d311497e2
Sasha <graduss@tut.by>
2014-03-13 22:04:46 +0300

    RBTree: 	add method btInsert, 	leftRoate, rightRotate, ~RBTree Item: 	add Item(const Item& i)

--- RBTree.h
+++ RBTree.h
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "smartpointer/SPointer.h"
 
 #ifndef RBTREE_H
@@ -27,11 +26,80 @@ private:
         SPointer<Item> l, r, p;
 
         Item(Data& k): key(k), l(0), r(0), p(0) {}
+        Item(const Item& i);
         ~Item();
     };
     SPointer<Item> root;
-};
 
+    void leftRotate(SPointer<Item>& x);
+    void rightRotate(SPointer<Item>& x);
+    SPointer<Item>& btInsert(SPointer<Item>& x);
+};
+/*******
+** RBTree
+**/
+template<class Data>
+SPointer<typename RBTree<Data>::Item>& RBTree<Data>::btInsert(SPointer<Item>& n){
+    SPointer<Item> root = this->root;
+    SPointer<Item>& x;
+
+    while(root){
+        x = root;
+        if(root->key > n->key) root = root->l;
+        else root = root->r;
+    }
+
+    n->p = x;
+    if (!x) this->root = n;
+    else if(x->key > n->key) x->l = n;
+    else x->r = n;
+
+    return n;
+}
+
+template<class Data>
+void RBTree<Data>::leftRotate(SPointer<Item>& x){
+    SPointer<Item> y = x->r;
+
+    x->r = y->l;
+    if(y->l) y->l->p = x;
+
+    y->p = x->p;
+
+    if(!x->p) root = y;
+    else if(x == x->p->l) x->p->l = y;
+    else x->p->r = y;
+
+    y->l = x;
+    x->p = y;
+}
+
+template<class Data>
+void RBTree<Data>::rightRotate(SPointer<Item>& x){
+    SPointer<Item> y = x->l;
+
+    x->l = y->r;
+    if(y->r) y->r->p = x;
+
+    y->p = x->p;
+
+    if(!x->p) root = y;
+    else if(x == x->p->l) x->p->l = y;
+    else x->p->r = y;
+
+    y->r = x;
+    x->p = y;
+}
+
+template<class Data>
+RBTree<Data>::~RBTree(){
+    if(root) delete &root;
+}
+/************************************************************/
+
+/*******
+**  Item
+**/
 template <class Data>
 RBTree<Data>::Item::~Item(){
     if(p){
@@ -44,6 +112,14 @@ RBTree<Data>::Item::~Item(){
     p = l = r = 0;
 }
 
+template<class Data>
+RBTree<Data>::Item::Item(const Item& i){
+    key = i.key;
+    l = i.l;
+    r = i.r;
+    p = i.p;
+}
+/************************************************************/
 /*class RBTree
 {
     public:
